1422fdda94ffe5ad8e50c040d1ab8f3fd215a8c9
phcaradanai <phcaradanai@gmail.com>
2025-01-07 16:04:32 +0700

    fix

--- lib/app/modules/history/bindings/history_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:xengistic_app/app/modules/history/controllers/history_controller.dart';
-
-class HistoryBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HistoryController());
-  }
-}
--- lib/app/services/utils.dart
+++
@@ -1,8 +0,0 @@
-bool containsMap(Map<dynamic, dynamic> mainMap, Map<dynamic, dynamic> subMap) {
-  for (var key in subMap.keys) {
-    if (!mainMap.containsKey(key) || mainMap[key] != subMap[key]) {
-      return false;
-    }
-  }
-  return true;
-}
